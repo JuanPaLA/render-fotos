724e82d2847216fe70c73fbae31bbc032c11edc8
JuanPaLA <juanlopezalurralde@gmail.com>
2021-08-14 13:08:05 -0300

    1.5

--- src/components/landingcontent.js
+++ src/components/landingcontent.js
@@ -37,7 +37,7 @@ export default function Content(props){
     return(
             <div>
             <Card body>
-                <CardTitle tag="h1" style={{ textAlign:'center'}}></CardTitle>
+                
                 <CardText style={{ padding:'0% 10% 0% 10%'}}> ¿De quién te enamorás para no crear? ¿Cuánto tarda? ¿Cuánto tiempo pasó? ¿Cuánto plástico te seduce? ¿Cuánto arte cabe en la gota de saliva compartida? ¿Qué se escurre por las comisuras? ¿Dónde reside le otre? ¿Cuándo lo ves sin verte? ¿Qué tan descartable es tu tiempo? ¿Qué te urge de deseo? ¿Qué no aparece en los sueños de otres? ¿Qué te decís en silencio? ¿Cuándo te cansaste del vacío? ¿En qué crees para dar cuerpo a alguien? ¿Cuándo se acaba la ficción? ¿Quién sos después? ¿Cómo creás sin amor?</CardText>
                 <div style={{textAlign:'center'}}>
                     <Link to={'/content'}>
@@ -51,6 +51,7 @@ export default function Content(props){
                         </Button>
                     </Link>
                 </div>
+                <CardText style={{ fontStyle:'italic', padding:'0% 10% 0% 10%', fontSize: '1.2em', marginTop:'2vh'}}>Una obra de Anabel Chaves del Pino</CardText>
                 </Card>
             </div>
     )
